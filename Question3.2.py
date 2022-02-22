8c1c5f9ae4ea7a50c558d7164f001a6818593829
Saiteja-Akkineni <99636695+Saiteja-Akkineni@users.noreply.github.com>
2022-02-22 22:34:33 +0530

    Delete Question3.2.py

--- Question3.2.py
+++
@@ -1,8 +0,0 @@
-sturec=[[1,"Arya",96],[2,"Arun",28],[3,"Iniyan",84],[4,"Tamil",61]]
-print("Roll.No|Name      |Marks |")
-size=len(sturec)
-p=5-len(str(sturec[1][0]))
-n=8-len(sturec[1][1])
-print("|",sturec[1][0]," "*p,end="|")
-print(sturec[1][1]," "*n,end="|")
-print(sturec[1][2]," "*3,end="| \n")
